5a81d0127814a5186de06e45a9cd67c02799a379
MisterTP <thierry.perotto@gmail.com>
2026-09-21 19:50:42 +0200

    Climat d’un coup : ZFE 14/04/2026 aussi Boccaletti + Olive

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 async function loadData() {
-  const v = "20260921d";
+  const v = "20260921e";
   const [b1Res, b2Res, b3Res, elusRes] = await Promise.all([
     fetch("data/batch-1.json?v=" + v),
     fetch("data/batch-2.json?v=" + v),
